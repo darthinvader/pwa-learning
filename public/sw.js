69184e065dc697c0df10d34cf00a4c964863ef77
darthinvader <psaradakisj@gmail.com>
2021-09-05 13:13:06 +0300

    [feat] returning from cache

--- public/sw.js
+++ public/sw.js
@@ -14,5 +14,13 @@ self.addEventListener("activate", function (event) {
 });
 
 self.addEventListener("fetch", function (event) {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      } else {
+        fetch(event.request);
+      }
+    })
+  );
 });
