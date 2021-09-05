734d914d2ef4e8a4615150ac54ac0d8bdba3467d
darthinvader <psaradakisj@gmail.com>
2021-09-05 13:01:50 +0300

    [feat] caching

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,18 @@
-self.addEventListener("install", function (event) {});
+self.addEventListener("install", function (event) {
+  console.log("[Service Worker] Installing Service Worker ...", event);
+  event.waitUntil(
+    caches.open("static").then(async (cache) => {
+      console.log("[Service Worker] Precaching App Shell");
+      await cache.add("/src/js/app.js");
+    })
+  );
+});
 
 self.addEventListener("activate", function (event) {
+  console.log("[Service Worker] Activating Service Worker ....", event);
   return self.clients.claim();
 });
 
-self.addEventListener("fetch", function (event) {});
+self.addEventListener("fetch", function (event) {
+  event.respondWith(fetch(event.request));
+});
