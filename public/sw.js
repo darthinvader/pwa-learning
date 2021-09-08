c98896268eb1098da52b4266101c6de2223d6e80
darthinvader <psaradakisj@gmail.com>
2021-09-08 21:02:33 +0300

    [feat] utlity for service works idb functions

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 importScripts("/src/js/idb.js");
-
+importScripts("src/js/utility.js");
 var CACHE_STATIC_NAME = "static-v16";
 var CACHE_DYNAMIC_NAME = "dynamic-v2";
 var STATIC_FILES = [
@@ -19,12 +19,6 @@ var STATIC_FILES = [
   "https://fonts.googleapis.com/icon?family=Material+Icons",
   "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css",
 ];
-
-var dbPromise = idb.open("posts-store", 1, function (db) {
-  if (!db.objectStoreNames.contains("posts")) {
-    db.createObjectStore("posts", { keyPath: "id" });
-  }
-});
 
 // function trimCache(cacheName, maxItems) {
 //   caches.open(cacheName)
@@ -86,12 +80,7 @@ self.addEventListener("fetch", function (event) {
         var clonedRes = res.clone();
         clonedRes.json().then(function (data) {
           for (var key in data) {
-            dbPromise.then(function (db) {
-              var tx = db.transaction("posts", "readwrite");
-              var store = tx.objectStore("posts");
-              store.put(data[key]);
-              return tx.complete;
-            });
+            writeData("posts", data[key]);
           }
         });
         return res;
