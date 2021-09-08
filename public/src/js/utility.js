2fb468c257c04e4d3a09f79fcf5256bc1c06c8f9
darthinvader <psaradakisj@gmail.com>
2021-09-08 21:26:37 +0300

    [feat] read from indexedDB

--- public/src/js/utility.js
+++ public/src/js/utility.js
@@ -12,3 +12,11 @@ const writeData = (st, data) => {
     return tx.complete;
   });
 };
+
+function readAllData(st) {
+  return dbPromise.then((db) => {
+    var tx = db.transaction(st, "readonly");
+    var store = tx.objectStore(st);
+    return store.getAll();
+  });
+}
