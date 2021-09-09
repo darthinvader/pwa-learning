5747e650fa2b721977147e56f9465e9af2f77679
darthinvader <psaradakisj@gmail.com>
2021-09-09 15:03:12 +0300

    [feat] updated files for 9th chapter

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -1,22 +1,26 @@
-var shareImageButton = document.querySelector("#share-image-button");
-var createPostArea = document.querySelector("#create-post");
-var closeCreatePostModalButton = document.querySelector(
-  "#close-create-post-modal-btn"
-);
-var sharedMomentsArea = document.querySelector("#shared-moments");
+var shareImageButton = document.querySelector('#share-image-button');
+var createPostArea = document.querySelector('#create-post');
+var closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn');
+var sharedMomentsArea = document.querySelector('#shared-moments');
+var form = document.querySelector('form');
+var titleInput = document.querySelector('#title');
+var locationInput = document.querySelector('#location');
 
 function openCreatePostModal() {
-  createPostArea.style.display = "block";
+  // createPostArea.style.display = 'block';
+  // setTimeout(function() {
+    createPostArea.style.transform = 'translateY(0)';
+  // }, 1);
   if (deferredPrompt) {
     deferredPrompt.prompt();
 
-    deferredPrompt.userChoice.then(function (choiceResult) {
+    deferredPrompt.userChoice.then(function(choiceResult) {
       console.log(choiceResult.outcome);
 
-      if (choiceResult.outcome === "dismissed") {
-        console.log("User cancelled installation");
+      if (choiceResult.outcome === 'dismissed') {
+        console.log('User cancelled installation');
       } else {
-        console.log("User added to home screen");
+        console.log('User added to home screen');
       }
     });
 
@@ -34,48 +38,49 @@ function openCreatePostModal() {
 }
 
 function closeCreatePostModal() {
-  createPostArea.style.display = "none";
+  createPostArea.style.transform = 'translateY(100vh)';
+  // createPostArea.style.display = 'none';
 }
 
-shareImageButton.addEventListener("click", openCreatePostModal);
+shareImageButton.addEventListener('click', openCreatePostModal);
 
-closeCreatePostModalButton.addEventListener("click", closeCreatePostModal);
+closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
 
 // Currently not in use, allows to save assets in cache on demand otherwise
 function onSaveButtonClicked(event) {
-  console.log("clicked");
-  if ("caches" in window) {
-    caches.open("user-requested").then(function (cache) {
-      cache.add("https://httpbin.org/get");
-      cache.add("/src/images/sf-boat.jpg");
-    });
+  console.log('clicked');
+  if ('caches' in window) {
+    caches.open('user-requested')
+      .then(function(cache) {
+        cache.add('https://httpbin.org/get');
+        cache.add('/src/images/sf-boat.jpg');
+      });
   }
 }
 
 function clearCards() {
-  while (sharedMomentsArea.hasChildNodes()) {
+  while(sharedMomentsArea.hasChildNodes()) {
     sharedMomentsArea.removeChild(sharedMomentsArea.lastChild);
   }
 }
 
 function createCard(data) {
-  var cardWrapper = document.createElement("div");
-  cardWrapper.className = "shared-moment-card mdl-card mdl-shadow--2dp";
-  var cardTitle = document.createElement("div");
-  cardTitle.className = "mdl-card__title";
-  cardTitle.style.backgroundImage = "url(" + data.image + ")";
-  cardTitle.style.backgroundSize = "cover";
-  cardTitle.style.height = "180px";
+  var cardWrapper = document.createElement('div');
+  cardWrapper.className = 'shared-moment-card mdl-card mdl-shadow--2dp';
+  var cardTitle = document.createElement('div');
+  cardTitle.className = 'mdl-card__title';
+  cardTitle.style.backgroundImage = 'url(' + data.image + ')';
+  cardTitle.style.backgroundSize = 'cover';
   cardWrapper.appendChild(cardTitle);
-  var cardTitleTextElement = document.createElement("h2");
-  cardTitleTextElement.style.color = "white";
-  cardTitleTextElement.className = "mdl-card__title-text";
+  var cardTitleTextElement = document.createElement('h2');
+  cardTitleTextElement.style.color = 'white';
+  cardTitleTextElement.className = 'mdl-card__title-text';
   cardTitleTextElement.textContent = data.title;
   cardTitle.appendChild(cardTitleTextElement);
-  var cardSupportingText = document.createElement("div");
-  cardSupportingText.className = "mdl-card__supporting-text";
+  var cardSupportingText = document.createElement('div');
+  cardSupportingText.className = 'mdl-card__supporting-text';
   cardSupportingText.textContent = data.location;
-  cardSupportingText.style.textAlign = "center";
+  cardSupportingText.style.textAlign = 'center';
   // var cardSaveButton = document.createElement('button');
   // cardSaveButton.textContent = 'Save';
   // cardSaveButton.addEventListener('click', onSaveButtonClicked);
@@ -92,16 +97,16 @@ function updateUI(data) {
   }
 }
 
-var url = "";
+var url = 'https://pwagram-99adf.firebaseio.com/posts.json';
 var networkDataReceived = false;
 
 fetch(url)
-  .then(function (res) {
+  .then(function(res) {
     return res.json();
   })
-  .then(function (data) {
+  .then(function(data) {
     networkDataReceived = true;
-    console.log("From web", data);
+    console.log('From web', data);
     var dataArray = [];
     for (var key in data) {
       dataArray.push(data[key]);
@@ -109,11 +114,25 @@ fetch(url)
     updateUI(dataArray);
   });
 
-if ("indexedDB" in window) {
-  readAllData("posts").then(function (data) {
-    if (!networkDataReceived) {
-      console.log("From cache", data);
-      updateUI(data);
-    }
-  });
+if ('indexedDB' in window) {
+  readAllData('posts')
+    .then(function(data) {
+      if (!networkDataReceived) {
+        console.log('From cache', data);
+        updateUI(data);
+      }
+    });
 }
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  if (titleInput.value.trim() === '' || locationInput.value.trim() === '') {
+    alert('Please enter valid data!');
+    return;
+  }
+
+  closeCreatePostModal();
+
+
+});
--- public/src/js/utility.js
+++ public/src/js/utility.js
@@ -1,44 +1,48 @@
-var dbPromise = idb.open("posts-store", 1, function (db) {
-  if (!db.objectStoreNames.contains("posts")) {
-    db.createObjectStore("posts", { keyPath: "id" });
+
+var dbPromise = idb.open('posts-store', 1, function (db) {
+  if (!db.objectStoreNames.contains('posts')) {
+    db.createObjectStore('posts', {keyPath: 'id'});
   }
 });
 
 function writeData(st, data) {
-  return dbPromise.then(function (db) {
-    var tx = db.transaction(st, "readwrite");
-    var store = tx.objectStore(st);
-    store.put(data);
-    return tx.complete;
-  });
+  return dbPromise
+    .then(function(db) {
+      var tx = db.transaction(st, 'readwrite');
+      var store = tx.objectStore(st);
+      store.put(data);
+      return tx.complete;
+    });
 }
 
 function readAllData(st) {
-  return dbPromise.then(function (db) {
-    var tx = db.transaction(st, "readonly");
-    var store = tx.objectStore(st);
-    return store.getAll();
-  });
+  return dbPromise
+    .then(function(db) {
+      var tx = db.transaction(st, 'readonly');
+      var store = tx.objectStore(st);
+      return store.getAll();
+    });
 }
 
 function clearAllData(st) {
-  return dbPromise.then(function (db) {
-    var tx = db.transaction(st, "readwrite");
-    var store = tx.objectStore(st);
-    store.clear();
-    return tx.complete;
-  });
+  return dbPromise
+    .then(function(db) {
+      var tx = db.transaction(st, 'readwrite');
+      var store = tx.objectStore(st);
+      store.clear();
+      return tx.complete;
+    });
 }
 
 function deleteItemFromData(st, id) {
-  return dbPromise
-    .then(function (db) {
-      var tx = db.transaction(st, "readwrite");
+  dbPromise
+    .then(function(db) {
+      var tx = db.transaction(st, 'readwrite');
       var store = tx.objectStore(st);
       store.delete(id);
       return tx.complete;
     })
-    .then(() => {
-      console.log("item deleted");
+    .then(function() {
+      console.log('Item deleted!');
     });
 }
