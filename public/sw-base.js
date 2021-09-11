c678fd10786c593c0e223ed0ac02bd35a176eb00
darthinvader <psaradakisj@gmail.com>
2021-09-11 13:40:54 +0300

    [feat] cache expiration

--- public/sw-base.js
+++ public/sw-base.js
@@ -6,6 +6,10 @@ workboxSW.router.registerRoute(
   /.*(?:googleapis|gstatic)\.com.*$/,
   workboxSW.strategies.staleWhileRevalidate({
     cacheName: "google-fonts",
+    cacheExpiration: {
+      maxEntries: 3,
+      maxAgeSeconds: 60 * 60 * 24 * 30,
+    },
   })
 );
 
